e75c62b9edf0e91ff078a19868f6cb76c414b749
James Williams <66931+jwill@users.noreply.github.com>
2025-03-19 19:40:23 -0700

    Update jni_utils.dart
    Remove unneeded classes

--- lib/jni_utils.dart
+++ lib/jni_utils.dart
@@ -5,36 +5,6 @@ import 'dart:ffi';
 import 'package:blood_pressure_app/health_connect/java/time/_package.dart';
 import 'package:jni/jni.dart';
 
-
-class JInstantExt {
-  static var clazz = JClass.forName('java/time/Instant');
-  late JObject jObject;
-  late JLong epochSecond;
-
-  static final JStaticMethodId _now = clazz.staticMethodId(r'now', r'()Ljava/time/Instant;');
-  static final JInstanceMethodId _epochMilli = clazz.instanceMethodId(r'toEpochMilli', r'()J');
-
-  static JObject now() {
-    var instant = _now.call(clazz, JObject.type, []);
-    return instant;
-  }
-
-  // Not working
-  static getEpochMilli(JObject instant) {
-    return _epochMilli(instant, JLong.type, []);
-  }
-
-  static ofEpochMilli(JLong milli) {
-    return clazz.staticMethodId(r'ofEpochMilli', r'(J)Ljava/time/Instant;').call(clazz, JObject.type, [milli.longValue()]);
-  }
-
-  JObject toJObject() {
-    return jObject;
-  }
-}
-
-
-
 ZoneOffset getZoneOffset() {
   // Get current time
   DateTime d = DateTime.now();
